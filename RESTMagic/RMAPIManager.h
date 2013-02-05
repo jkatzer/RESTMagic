689a40971228c52e25afd86232a84d952b9a8a13
jason <json@katzr.com>
2013-02-04 18:07:20 -0800

    adding back last version or RMAPIManager

---
+++ RESTMagic/RMAPIManager.h
@@ -0,0 +1,30 @@
+//
+//  RMAPIManager.h
+//  RESTMagic
+//
+//  Created by Jason Katzer on 7/7/12.
+//  Copyright (c) 2012 Jason Katzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RMViewController.h"
+
+@class RMViewController;
+
+@interface RMAPIManager : NSObject
+{
+    NSURL* baseURL;
+}
+
+@property (nonatomic, retain) NSURL* baseURL;
+
++(RMAPIManager *)sharedAPIManager;
+-(NSString *)nameForResourceAtPath:(NSString *)path;
+-(NSString *)nameForResourceAtURL:(NSURL *)url;
+-(NSURL *)URLForResourceAtPath:(NSString *)path;
+-(NSString *)urlForResourceAtPath:(NSString *)path;
+-(NSString *)templateUrlForResourceAtUrl:(NSURL *)url;
+-(RMViewController *)viewControllerForResourceAtPath:(NSString *)path;
+-(RMViewController *)viewControllerForResourceAtURL:(NSURL *)url;
+
+@end
