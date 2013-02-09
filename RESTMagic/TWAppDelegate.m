8c9a5e772306d97538e8bc5ca49ef6b85a42460e
jason <json@katzr.com>
2013-02-09 03:24:35 -0800

    removing more apple sample code and files

--- RESTMagic/TWAppDelegate.m
+++ RESTMagic/TWAppDelegate.m
@@ -8,28 +8,19 @@
 
 #import "TWAppDelegate.h"
 #import "RMAPIManager.h"
-#import "RMFirstViewController.h"
-#import "RMSecondViewController.h"
 
 @implementation TWAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    UIViewController *viewController1 = [[RMFirstViewController alloc] initWithNibName:@"RMFirstViewController" bundle:nil];
-    UIViewController *viewController2 = [[RMSecondViewController alloc] initWithNibName:@"RMSecondViewController" bundle:nil];
-    
-    
     
     RMAPIManager *apiManager = [RMAPIManager sharedAPIManager];    
     
     self.navigationController = [[UINavigationController alloc] initWithRootViewController:[apiManager viewControllerForResourceAtPath:@"trends/daily.json"]];
 
-    
-    
     self.tabBarController = [[UITabBarController alloc] init];
-    self.tabBarController.viewControllers = @[viewController1, viewController2, self.navigationController];
+    self.tabBarController.viewControllers = @[self.navigationController];
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
     return YES;
