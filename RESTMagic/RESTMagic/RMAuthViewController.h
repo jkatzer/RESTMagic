019783ba832fa14b5d68909b58d1bdd23b978978
jason <json@katzr.com>
2013-02-20 21:05:59 -0500

    need to add new files to here for using pods. won't build this commit

---
+++ RESTMagic/RESTMagic/RMAuthViewController.h
@@ -0,0 +1,14 @@
+//
+//  RMAuthViewController.h
+//  RESTMagic
+//
+//  Created by Jason Katzer on 2/20/13.
+//
+//
+
+#import "RMViewController.h"
+#import "RMWebViewDelegate.h"
+
+@interface RMAuthViewController : RMViewController <RMWebViewDelegate>
+
+@end
---
+++ RESTMagic/RESTMagic/RMAuthViewController.m
@@ -0,0 +1,13 @@
+//
+//  RMAuthViewController.m
+//  RESTMagic
+//
+//  Created by Jason Katzer on 2/20/13.
+//
+//
+
+#import "RMAuthViewController.h"
+
+@implementation RMAuthViewController
+
+@end
