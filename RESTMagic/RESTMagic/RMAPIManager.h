b2bc9b5cb8c66d830154470e9e35d3a1b93346e4
jason <json@katzr.com>
2013-02-24 16:02:44 -0500

    fixing all xcode warnings

--- RESTMagic/RESTMagic/RMAPIManager.h
+++ RESTMagic/RESTMagic/RMAPIManager.h
@@ -2,24 +2,18 @@
 //  RMAPIManager.h
 //  RESTMagic
 //
-//  Created by Jason Katzer on 7/7/12.
-//  Copyright (c) 2012 Jason Katzer. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import "RMViewController.h"
 #import "RMAuthViewController.h"
 
 @interface RMAPIManager : NSObject
-{
-    NSURL* baseURL;
-    NSDictionary* settings;
-}
 
 @property (nonatomic, retain) NSURL* baseURL;
+@property (nonatomic, retain, readonly) NSDictionary* settings;
 
 +(RMAPIManager *)sharedAPIManager;
--(NSDictionary*)settings;
 -(NSString *)nameForResourceAtPath:(NSString *)path;
 -(NSString *)nameForResourceAtURL:(NSURL *)url;
 -(NSURL *)URLForResourceAtPath:(NSString *)path;
